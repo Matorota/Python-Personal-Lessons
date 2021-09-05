a121a668b8cfc94bec8c14358805abc1024eaabc
unknown <matasmatasp@gmail.com>
2021-09-05 11:08:10 +0300

    1PaskaitaDone

---
+++ 1-pask/main.py
@@ -0,0 +1,30 @@
+hello_world_text = "Hello word!!!"
+print(hello_world_text)
+
+URL = "htttps://www.google.lt"
+
+my_name = "Matas"
+my_age = 17
+my_school = "Zemyna"
+my_interests = "Airsoft"
+my_favorite_game = "Hearts of iron 4"
+
+
+print("Hello, my name is: " + my_name)
+print("My age: " + str(my_age))
+print("School: " + my_school)
+print(f"My interests: {my_interests}" + my_interests)
+print(f"My favorite game: {my_favorite_game}" )
+
+print(my_name, my_age, my_school, sep="-")
+print(my_name.upper())
+print(my_name.lower())
+
+number1 = 1
+number2 = 2
+result = number1 + number2
+print(result)
+
+
+#ctrl + click (str ar kitu kodas)
+#alt+ j (pasirinkti kelis)
