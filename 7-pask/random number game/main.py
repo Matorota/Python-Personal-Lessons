6d84f4883ded5b39e250cb2de5c1d40f3b8ed6e5
Matorota <78918727+Matorota@users.noreply.github.com>
2021-09-27 18:59:19 +0300

    fixes

--- 7-pask/random number game/main.py
+++ 7-pask/random number game/main.py
@@ -1,17 +1,28 @@
+# from game_modes import Easy
+#
+# game_modes = Easy()
+#
+# is_on = True
+#
+# while is_on:
+#     choice = input(f"Levels:  Easy , Medium , Hard: \n Entr levels name: ")
+#
+#     if choice == "Easy":
+#         game_modes.play()
+#     elif choice == "off":
+#         is_on = False
+#
+#     else:
+#         print("there is no such level")
+
 from game_modes import Easy
-
 game_modes = Easy()
-
 is_on = True
 
 while is_on:
-    choice = input(f"Levels:  Easy , Medium , Hard: \n Entr levels name: ")
-
-    if choice == "Easy":
-        game_modes.play()
-    elif choice == "off":
+    choice = input("Write START to play")
+    if choice == "off":
         is_on = False
-
-    else:
-        print("there is no such level")
+    elif choice == "START":
+        game_modes.play()
 
