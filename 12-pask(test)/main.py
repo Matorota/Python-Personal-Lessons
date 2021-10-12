b141a53978dde9269bd5ea2d66adcf84c7e9c2be
Matorota <matasmatasp@gmail.com>
2021-10-12 17:47:42 +0300

    12-pask

---
+++ 12-pask(test)/main.py
@@ -0,0 +1,15 @@
+from question_bank import QuestionBank
+from question_bank import Quesetion
+
+question_bank = QuestionBank()
+single_q = Quesetion()
+
+is_on = True
+
+while is_on:
+    print("")
+
+    # options = question_bank.get_category()
+    # choice = input(f"?({options})")
+
+
